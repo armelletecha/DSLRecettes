4bbe3f6ba99ddb5a1b43900c2e1559a954960f4a
armelletecha <armelle.tenekeu-chibemen.1@ens.etsmtl.ca>
2016-12-20 20:31:15 -0500

    DSL sans les interfaces builder de Ingredient et Etape

--- src/main/java/uqam/mgl7460/projet3/dsl/recettes/EtapeBuilder.java
+++
@@ -1,5 +0,0 @@
-package uqam.mgl7460.projet3.dsl.recettes;
-
-interface EtapeBuilder {
-	void build (Etape e); 
-	}
--- src/main/java/uqam/mgl7460/projet3/dsl/recettes/IngredientBuilder.java
+++
@@ -1,5 +0,0 @@
-package uqam.mgl7460.projet3.dsl.recettes;
-
-interface IngredientBuilder {
-	void build (Ingredient i); 
-	}
